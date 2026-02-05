ec1013edddee01917db0ad421e62cc591ed3db50
aaddyaaa <adyaaryasingh@gmail.com>
2026-02-05 23:40:19 +0530

    Fix OMDB env var and run app locally

--- app.py
+++ app.py
@@ -1,54 +1,55 @@
-
 import streamlit as st
 import pickle
 import pandas as pd
-
+import os
 import requests
 
-def fetch_poster_by_title(title):
-    url = f"http://www.omdbapi.com/?t={title}&apikey=965d53a0"
-    data = requests.get(url).json()
+OMDB_API_KEY = os.environ.get("OMDB_API_KEY")
+if not OMDB_API_KEY:
+    raise RuntimeError("OMDB_API_KEY not set")
 
+@st.cache_data(show_spinner=False)
+def fetch_poster_by_title(title):
+    url = f"http://www.omdbapi.com/?t={title}&apikey={OMDB_API_KEY}"
+    data = requests.get(url).json()
     if data.get("Response") == "True" and data.get("Poster") != "N/A":
         return data["Poster"]
     return None
 
-movies = pd.read_csv("tmdb_5000_movies.csv")
+movies_dict = pickle.load(open('movies_dict.pkl', 'rb'))
+movies = pd.DataFrame(movies_dict)
+similarity = pickle.load(open('similarity.pkl', 'rb'))
 
 def recommend(movie):
+    if movie not in movies['title'].values:
+        return []
+
     movie_index = movies[movies['title'] == movie].index[0]
     distances = similarity[movie_index]
+
     movies_list = sorted(
         list(enumerate(distances)),
         reverse=True,
         key=lambda x: x[1]
     )[1:6]
 
-    recommended_movies = []
-
+    recommendations = []
     for i in movies_list:
         title = movies.iloc[i[0]].title
-        poster = fetch_poster_by_title(title)  # ← NEW LINE
-        recommended_movies.append((title, poster))  # ← CHANGE
+        poster = fetch_poster_by_title(title)
+        recommendations.append((title, poster))
 
-    return recommended_movies
+    return recommendations
 
-
-movies_dict = pickle.load(open('movies_dict.pkl', 'rb'))
-
-similarity = pickle.load(open('similarity.pkl', 'rb'))
-
-st.title('Movie Recommender System')
+st.title("Movie Recommender System")
 
 selected_movie_name = st.selectbox(
-    'Select a movie',
+    "Select a movie",
     movies['title'].values
 )
 
-if st.button('Recommend'):
-    recommendations = recommend(selected_movie_name)
-
-    for title, poster in recommendations:
+if st.button("Recommend"):
+    for title, poster in recommend(selected_movie_name):
         if poster:
             st.image(poster, width=200)
         st.write(title)
